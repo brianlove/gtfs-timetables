a05a9f1c04295081154df5d1630784fd92ec9d20
Brian Love <project-timetable@brianlove.dev>
2022-05-01 21:28:01 -0400

    Move Route transformations to API container
    Move the rest of the API transformations to the API container.  Clean
    up the GUI's wrapper file.

--- api/src/controllers/routes.controller.ts
+++ api/src/controllers/routes.controller.ts
@@ -1,7 +1,12 @@
 
+import { Request, Response } from 'express';
 import { Op } from 'sequelize';
 
 import { Calendar, RouteModel, StopTime, TripModel } from '../models';
+
+import type { Route, Schedule, Train, Trip, TripStop } from '@/types';
+import { RawGtfsRouteAndTrips } from '@/types/raw';
+import { convertRawTrip } from '../util-from-gui/conversions';
 
 
 function findOne(req, res) {
@@ -69,10 +74,16 @@ function findRouteStations(req, res) {
 }
 
 
-function findRouteTrips(req, res) {
+interface TrainMapping {
+    [index: string]: Train,
+}
+
+
+async function findRouteTrips(req: Request, res: Response) {
     const id = req.params.id;
 
-    RouteModel.findByPk(id, {
+    try {
+        const rawRoute = await RouteModel.findByPk(id, {
             include: [
                 {
                     model: TripModel,
@@ -94,16 +105,59 @@ function findRouteTrips(req, res) {
                     ],
                 },
             ],
-        })
-        .then((data) => {
-            res.send(data);
-        })
-        .catch((err) => {
-            res.status(500).send({
-                message: "Error while retrieving route trips",
-                error: err.message,
-            });
         });
+
+        if ( rawRoute === null ) {
+            res.status(404).send({
+                message: `No route found: ${id}`,
+            });
+        } else {
+            const jsonRoute = rawRoute.toJSON() as RawGtfsRouteAndTrips;
+            const { trips, ...details } = jsonRoute;
+
+            const routeId = parseInt(id);
+            const agencyId = parseInt(details.agency_id || '');
+            const convertedTrips = trips.map(convertRawTrip);
+
+            const trainMapping = {} as TrainMapping;
+            convertedTrips.forEach((trip: Trip) => {
+                const trainId = parseInt(trip.shortName || '');
+
+                if ( trainMapping.hasOwnProperty(trainId) ) {
+                    trainMapping[trainId].trips.push(trip);
+                } else {
+                    trainMapping[trainId] = {
+                        trainId,
+                        routeId,
+                        agencyId,
+                        name: details.route_short_name || details.route_long_name || '',
+                        direction: trip.direction,
+                        trips: [trip],
+                    } as Train;
+                }
+            });
+
+            const route = {
+                routeId,
+                agencyId,
+                shortName: details.route_short_name,
+                longName: details.route_long_name,
+                desc: details.route_desc,
+                type: details.route_type,
+                url: details.route_url,
+                color: details.route_color,
+                textColor: details.route_text_color,
+                trains: Object.values(trainMapping),
+            } as Route;
+
+            res.send(route);
+        }
+    } catch (error) {
+        res.status(500).send({
+            message: "Error while retrieving route trips",
+            error: error,
+        });
+    }
 }
 
 
--- gui-vue3/src/api.ts
+++ gui-vue3/src/api.ts
@@ -18,11 +18,6 @@ function getRoute(routeId: string) {
 }
 
 
-interface TrainMapping {
-    [index: string]: Train,
-}
-
-
 /**
  * Return the details of a `Route`, including an array of `Train`s and the
  * `Trip`s that form them.
@@ -30,98 +25,11 @@ interface TrainMapping {
 function getRouteAndTrips(routeId: number) : Promise<Route> {
     return fetch(`${API_URL}/routes/${routeId}/detailsAndTrips`)
         .then(response => response.json())
-        .then((rawResults: RawGtfsRouteAndTrips) => {
-            const { trips, ...details } = rawResults;
-
-            const agencyId = parseInt(details.agency_id || '');
-            const convertedTrips = trips.map(convertRawTrip);
-
-            const trainMapping = {} as TrainMapping;
-            convertedTrips.forEach((trip: Trip) => {
-                const trainId = parseInt(trip.shortName || '');
-
-                if ( trainMapping.hasOwnProperty(trainId) ) {
-                    trainMapping[trainId].trips.push(trip);
-                } else {
-                    trainMapping[trainId] = {
-                        trainId,
-                        routeId,
-                        agencyId,
-                        name: details.route_short_name || details.route_long_name || '',
-                        direction: trip.direction,
-                        trips: [trip],
-                    } as Train;
-                }
-            });
-
-            console.info("Train mapping:", trainMapping); // DEBUG
-
-            const trains = Object.values(trainMapping);
-
-            console.info("Converted to array:", trains); // DEBUG
-
-            return {
-                routeId,
-                agencyId,
-                shortName: details.route_short_name,
-                longName: details.route_long_name,
-                desc: details.route_desc,
-                type: details.route_type,
-                url: details.route_url,
-                color: details.route_color,
-                textColor: details.route_text_color,
-                trains: Object.values(trainMapping),
-            } as Route;
-        })
+        .then(data => data as Route)
         .then((response) => {
             console.info("getRouteAndTrips response:", response); // DEBUG
             return response;
         });
-}
-
-
-/**
- * Convert the GTFS/database format of a trip into our internal `Trip` format.
- */
-function convertRawTrip(rawTrip: RawGtfsTripExtended) : Trip {
-    const { stop_times, calendar, ...basics } = rawTrip;
-
-    const tripId = parseInt(basics.trip_id);
-    const routeId = parseInt(basics.route_id);
-
-    const tripStops = stop_times.map((entry: RawGtfsStopTime) => {
-        return {
-            arrivalTime: new NonBoundedTime(entry.arrival_time),
-            departureTime: new NonBoundedTime(entry.departure_time),
-            stopId: entry.stop_id,
-            sequence: entry.stop_sequence,
-            headsign: entry.stop_headsign || '',
-            pickupType: entry.pickup_type,
-            dropoffType: entry.drop_off_type,
-        } as TripStop;
-    });
-
-    const schedule = {
-        monday: calendar.monday == 'available',
-        tuesday: calendar.tuesday == 'available',
-        wednesday: calendar.wednesday == 'available',
-        thursday: calendar.thursday == 'available',
-        friday: calendar.friday == 'available',
-        saturday: calendar.saturday == 'available',
-        sunday: calendar.sunday == 'available',
-    } as Schedule;
-
-    return {
-        tripId: tripId,
-        routeId: routeId,
-        serviceStartDate: calendar.start_date,
-        serviceEndDate: calendar.end_date,
-        schedule: schedule,
-        headsign: basics.trip_headsign,
-        shortName: basics.trip_short_name,
-        direction: basics.direction_id,
-        stops: tripStops,
-    } as Trip;
 }
 
 
