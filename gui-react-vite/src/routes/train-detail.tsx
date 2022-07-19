5edf222c74379165e99f8146584c94b49964f41c
Brian Love <project-timetable@brianlove.dev>
2022-07-19 14:24:49 -0400

    WIP: start working on train detail - have CORS issue

--- gui-react-vite/src/routes/train-detail.tsx
+++ gui-react-vite/src/routes/train-detail.tsx
@@ -1,12 +1,33 @@
 
+import React, { useState, useEffect } from 'react';
 import { useParams } from "react-router-dom";
 
-export default function Train() {
+import { getTrainDetail } from '../api';
+
+export default function TrainDetail() {
   let { trainId } = useParams();
+
+  const [trainDetail, setTrainDetail] = useState({})
+
+  const fetchDetail = async () => {
+    const trainIdNumber = parseInt(trainId || "");
+    const detail = await getTrainDetail(trainIdNumber);
+    console.info("train detail:", detail);
+    setTrainDetail(detail);
+  }
+
+  useEffect(() => {
+    fetchDetail();
+  }, [])
+
 
   return (
     <main>
       <h2>Train {trainId}</h2>
+
+      {/* <div>
+        {trainDetail}
+      </div> */}
     </main>
   );
 }
